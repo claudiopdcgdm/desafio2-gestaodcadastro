d6711050918ac0695cd3d86c4e7ff4205eb5e3bd
Claudio Morais <claudiopdcgdm@gmail.com>
2021-07-17 21:49:13 -0300

    update url api products from http for https//

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 //'http://claudiopdcgdm.pythonanywhere.com/api/data-produtos/'
 
 const api = axios.create({
-    baseURL:'http://claudiopdcgdm.pythonanywhere.com'
+    baseURL:'https://claudiopdcgdm.pythonanywhere.com'
 })
 
 export default api
